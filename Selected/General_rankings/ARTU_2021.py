faa9377f7543fd57f8f6be416b32c9aabc3cccc6
chris <cmor5434@uni.sydney.edu.au>
2022-09-10 22:50:51 +1000

    Adding ARTU data

---
+++ Selected/General_rankings/ARTU_2021.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_html("http://research.unsw.edu.au/artu/artu-results")[0]
+df.drop("Unnamed: 0", axis=1, inplace=True)
+#print(df.info())
+
+artu_ranks = df[["University Name", "Country",  "ARTU Rank"]]
+#print(artu_ranks.shape)
+artu_ranks.to_csv("ARTU/ARTU_Ranks.csv", index=False)
+
+df_grouped = df.groupby("Country")["ARTU Rank"].describe().sort_values("25%")
+#print(df_grouped.info())
+df_grouped.to_csv("ARTU/ARTU_Country.csv")
