2a39866d0465ff8f885a41e8d2819035c569ac35
chris <cmor5434@uni.sydney.edu.au>
2022-09-11 22:05:21 +1000

    Fixing QS files

--- Selected/General_rankings/QS_2022.py
+++ Selected/General_rankings/QS_2022.py
@@ -6,34 +6,29 @@ import seaborn as sns
 
 ## Importing / Removing unwanted columns
 df = pd.read_excel(os.path.abspath("QS/QS2022.xlsx"), header=3, index_col=2)
-df_dropped = df.drop(columns=['rank in country', 'rank in subregion', 'rank display2', 'country code', 'ar rank', 'er rank', 'fsr rank', 'cpf rank', 'ifr rank', 'isr rank'])
+df_dropped = df.drop(columns=['rank in country', 'rank in subregion', 'rank display2',
+        'country code', 'ar rank', 'er rank', 'fsr rank', 'cpf rank', 'ifr rank', 'isr rank'])
 
-## Cleaning - Datatypes - issue with '-' in overall score; size bins now interpreted correctly
+## Cleaning - issue with '-' in overall score; size bins now interpreted correctly
 df_dropped['age band'] = df_dropped['age band'].astype(object)
-df_dropped.replace('-', np.NaN, inplace = True) # Impute with value? (<25)
+df_dropped.replace('-', np.NaN, inplace = True)
 df_dropped['score scaled'] = df_dropped['score scaled'].astype(float)
 df_dropped.to_csv("QS/QS2022_cleaned.csv", index=False)
-#print(df_dropped.info())
-
-## General summary
-#print(df.columns)
-#print(df.head())
-#print(df.info())
-#print(df.describe())
-
 
 ## Grouping by country - Comparing overall scores
-df_grouped = df_dropped.dropna().groupby("country")[['ar score', 'er score', 'fsr score', 'cpf score', 'ifr score', 'isr score', 'score scaled']]
+df_grouped = df_dropped.dropna().groupby("country")[['ar score', 'er score', 'fsr score',
+        'cpf score', 'ifr score', 'isr score', 'score scaled']]
 df_country_mean = df_grouped.mean()
 df_country_mean.to_csv("QS/QS2022_bycountry.csv")
 
 
-## Analysis of overall score (update)
-#print(df_grouped.mean())
-#print(df_grouped.describe().sort_values('count', ascending=False)) # No. universities in top 500
-#print(df_grouped.describe().sort_values('mean', ascending=False)) # Sorted by mean
-#print(df_grouped.describe().sort_values('50%', ascending=False)) # Sorted by median
+## Analysis
+# Simple histogram of overall scores
+sns.histplot(data=df_dropped.dropna(), x='score scaled')
+plt.show()
 
-## Simple histogram of overall scores
-#sns.histplot(data=df_dropped.dropna(), x='score scaled')
-#plt.show()
+# Viewing the 10 highest ranking universities:
+print(df_dropped.head(10))
+
+# Viewing the 5 highest average final scores of each country:
+print(df_dropped.groupby("country")["score scaled"].describe().sort_values("mean", ascending=False).head())
--- Selected/General_rankings/QS_2023.py
+++ Selected/General_rankings/QS_2023.py
@@ -4,34 +4,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 ## Importing / Removing unwanted columns
-#df = pd.read_csv("QS/QS World University Rankings 2022.csv")
 df = pd.read_excel("QS/QS2023.xlsx", header=3, index_col=0)
-df_dropped = df.drop(columns=['rank display2', 'location code', 'ar rank', 'er rank', 'fsr rank', 'cpf rank', 'ifr rank', 'isr rank', 'irn rank', 'ger rank'])
+df_dropped = df.drop(columns=['rank display2', 'location code', 'ar rank', 'er rank', 'fsr rank',
+                              'cpf rank', 'ifr rank', 'isr rank', 'irn rank', 'ger rank'])
 
-## Cleaning - Datatypes - issue with '-' in overall score; size bins now interpreted correctly
+
+## Cleaning - issue with '-' in overall score; size bins now interpreted correctly
 df_dropped['age band'] = df_dropped['age band'].astype(object)
 df_dropped.replace('-', np.NaN, inplace = True)
 df_dropped['score scaled'] = df_dropped['score scaled'].astype(float)
 df_dropped.to_csv("QS/QS2023_cleaned.csv", index=False)
-print(df_dropped.info())
 
-## General summary
-'''print(df.columns)
-print(df.head())
-print(df.info())
-print(df.describe())'''
 
 ## Grouping by country - Comparing overall scores
-df_grouped = df_dropped.dropna().groupby("location")[['ar score', 'er score', 'fsr score', 'cpf score', 'ifr score', 'isr score', 'irn score', 'ger score', 'score scaled']]
+df_grouped = df_dropped.dropna().groupby("location")[['ar score', 'er score', 'fsr score',
+            'cpf score', 'ifr score', 'isr score', 'irn score', 'ger score', 'score scaled']]
 df_country_mean = df_grouped.mean()
+
+
+## Writing to csv file
 df_country_mean.to_csv("QS/QS2023_bycountry.csv")
 
-#print(df_grouped.describe().sort_values('count', ascending=False)) # No. universities in top 500
-#print(df_grouped.describe().sort_values('mean', ascending=False)) # Sorted by mean
-#print(df_grouped.describe().sort_values('50%', ascending=False)) # Sorted by median
-
-## Simple histogram of overall scores
-#sns.histplot(data=df_dropped.dropna(), x='score scaled')
-#plt.show()
 
 
+## Analysis
+# Simple histogram of overall scores for each university
+sns.histplot(data=df_dropped.dropna(), x='score scaled')
+plt.show()
+
+# Viewing the 10 highest ranking universities:
+print(df_dropped.head(10))
+
+# Viewing the 5 highest average final scores of each country:
+print(df_dropped.groupby("location")["score scaled"].describe().sort_values("mean", ascending=False).head())
