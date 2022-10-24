06e5e71a019a24f2a7ee2360924f57052db00539
chris <cmor5434@uni.sydney.edu.au>
2022-10-25 04:11:45 +1100

    adding RandomForests + fixed datset

---
+++ Stage3/520419832/train_model.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# Selecting columns
+df = pd.read_csv("integrated_dataset.csv").dropna()
+X = df[["QS2023 Academic Reputation", "QS2023 Employer Reputation", "QS2023 Faculty Student Ratio",
+        "QS2023 Citations per Faculty", "QS2023 International Faculty Ratio"]]
+y = df["THE2020 Overall Score"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state = 42)
+
+rf = RandomForestRegressor(n_jobs=-1, bootstrap=True, max_depth=80, max_features=3, min_samples_leaf=3,
+                            min_samples_split=8, n_estimators=1000)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error: {:.2f}".format(mse))
+print("Root Mean Squared Error: {:.2f}".format(math.sqrt(mse)))
+
+importances_rf = pd.Series(rf.feature_importances_, index=X.columns).sort_values()
+importances_rf.plot(kind="barh", color="lightgreen")
+plt.show()
--- Stage3/model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-df = pd.read_csv("university_rankings_cleaned.csv")
-
-
-print(df["ARWU2018 Academic Performance per Capita"].dropna())
-
