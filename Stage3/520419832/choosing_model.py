d3dfd9f40ce4556d7fa60f202e605523f1cd91d7
chris <cmor5434@uni.sydney.edu.au>
2022-10-30 07:25:13 +1100

    Finalising model

--- Stage3/520419832/choosing_model.py
+++ Stage3/520419832/choosing_model.py
@@ -1,18 +1,26 @@
+'''
+Choosing the ideal hyper-parameters of the model
+SID: 520419832
+
+This code uses sklearn's GridSearchCV function to determine the ideal hyper-parameters to use for the
+final random forest predictive model, found in "train_model.py".
+
+'''
+
+## Imports
 import math
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-# Selecting columns
+## Reading the dataset and selecting columns
 df = pd.read_csv("integrated_dataset.csv").dropna()
-
-X = df[["QS2023 Academic Reputation", "QS2023 Employer Reputation", "QS2023 Faculty Student Ratio", "QS2023 Citations per Faculty", "QS2023 International Faculty Ratio"]]
+X = df[["QS2023 Academic Reputation", "QS2023 Employer Reputation", "QS2023 Faculty Student Ratio",
+    "QS2023 Citations per Faculty", "QS2023 International Faculty Ratio", "QS2023 International Student Ratio"]]
 y = df["THE2020 Overall Score"]
 
-# Parameter grid
+## Parameter grid of hyper-parameter values to test
 param_grid = {
     "max_depth": [60, 70, 80, 90, 100],
     "max_features": [2, 3, 4, 5],
@@ -21,12 +29,13 @@ param_grid = {
     "n_estimators": [100, 200, 500, 1000, 1500, 2000]
 }
 
-
+## Dividing the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state = 42)
 
+## Running the 5-fold GridSearchCV
 rf = RandomForestRegressor()
-
 grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=10)
 grid_search.fit(X_train, y_train)
 
+## Printing the best hyper-parameters to use in the final model
 print(grid_search.best_params_)
--- Stage3/520419832/train_model.py
+++ Stage3/520419832/train_model.py
@@ -1,34 +1,59 @@
+'''
+Training the Final Model
+SID: 520419832
+
+This code implements the optimal hyper-parameters specified in the choosing_model.py file, and scores
+the accuracy/effectiveness of the model using a variety of measurements.
+
+'''
+
+## Imports
 import math
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
-# Selecting columns
+## Reading the file & selecting columns
 df = pd.read_csv("integrated_dataset.csv").dropna()
 X = df[["QS2023 Academic Reputation", "QS2023 Employer Reputation", "QS2023 Faculty Student Ratio",
-    "QS2023 Citations per Faculty", "QS2023 International Faculty Ratio"]]
+    "QS2023 Citations per Faculty", "QS2023 International Faculty Ratio", "QS2023 International Student Ratio"]]
 y = df["THE2020 Overall Score"]
 
+
+## Splitting the data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state = 42)
 
-rf = RandomForestRegressor(n_jobs=-1, max_depth=60, max_features=3, min_samples_leaf=2,
-                            min_samples_split=8, n_estimators=100)
+
+## Creating and training the model
+rf = RandomForestRegressor(n_jobs=-1, max_depth=90, max_features=3, min_samples_leaf=2,
+                            min_samples_split=8, n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
+
+
+## Scoring the data
 y_pred = rf.predict(X_test)
 
-print("training data")
+# Training dataset results (supplementary)
+print("Training Dataset:")
 mse = mean_squared_error(y_train, rf.predict(X_train))
-print("Mean Squared Error: {:.2f}".format(mse))
-print("Root Mean Squared Error: {:.2f}".format(math.sqrt(mse)))
+print("Mean Squared Error: {:.5f}".format(mse))
+print("Root Mean Squared Error: {:.5f}".format(math.sqrt(mse)))
+print("R^2 Score: {:.5f}".format(r2_score(y_train, rf.predict(X_train))))
 
-print("testing data")
+print()
+
+# Testing dataset results
+print("Testing Dataset:")
 mse = mean_squared_error(y_test, y_pred)
-print("Mean Squared Error: {:.2f}".format(mse))
-print("Root Mean Squared Error: {:.2f}".format(math.sqrt(mse)))
+print("Mean Squared Error: {:.5f}".format(mse))
+print("Root Mean Squared Error: {:.5f}".format(math.sqrt(mse)))
+print("R^2 Score: {:.5f}".format(r2_score(y_test, y_pred)))
 
+
+## Graphing feature importances
 importances_rf = pd.Series(rf.feature_importances_, index=X.columns).sort_values()
-importances_rf.plot(kind="barh", color="lightgreen")
+ax = importances_rf.plot(kind="barh", color="lightgreen", title="Importance of QS2023 Metrics")
+ax.set_xlabel("Proportional feature importance") 
 plt.show()
