5b197d5d6300425b7d2a35dce7afd44e6579194b
chris <cmor5434@uni.sydney.edu.au>
2022-11-06 01:05:01 +1100

    Finalising model

--- Stage4/520419832/main.py
+++ Stage4/520419832/main.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 from bokeh.layouts import column, row
 from bokeh.plotting import figure, curdoc
-from bokeh.models import ColumnDataSource, Select, CustomJS, Button 
+from bokeh.models import ColumnDataSource, Select
 from bokeh.palettes import Spectral5
 from bokeh.transform import factor_cmap
 
@@ -24,7 +24,6 @@ df["QS2023 Overall Score"] = round(df["QS2023 Academic Reputation"]*0.4 + df["QS
 
 df["QS2023 Overall Score"] = round(df["QS2023 Overall Score"], 2)
 
-
 # Organising columns
 cols = sorted(df.columns)
 categorical_cols = [x for x in cols if df[x].dtype == object]
@@ -33,12 +32,13 @@ categorical_cols.remove("location")
 numerical_cols = [x for x in cols if df[x].dtype == "float64"]
 
 # Possible colours and sizes to use UPDATE
-COLORS = Spectral5
-N_COLORS = len(COLORS)
+COLOURS = Spectral5
 
 TOOLTIPS = [
     ("Name", "@institution"),
     ("Country", "@location"),
+    ("Age", "@age"),
+    ("Size", "@size"),
     ("x", "$x"),
     ("y", "$y"),
 ]
@@ -56,28 +56,29 @@ def create_figure():
         keywords['y_range'] = sorted(set(df[y.value].values))
     
     # Creation of figure
-    p = figure(height=700, width=700, tools="pan,wheel_zoom,box_zoom,hover,reset",
+    p = figure(height=700, width=700, tools="pan,wheel_zoom,box_zoom,hover",
                title="Comparison Between QS2023 & THE2020 Data", **keywords, tooltips=TOOLTIPS)
     
     # Sets axis labels
     p.xaxis.axis_label = x.value.title()
     p.yaxis.axis_label = y.value.title()
-
-    if x.value in categorical_cols:
-        p.xaxis.major_label_orientation = np.pi / 4
     
     # Update colours
-    if color.value != 'None':
-        colors = factor_cmap(color.value, palette=Spectral5, factors=df[color.value].unique())
-    
+    if colour.value != 'None':
+        colours = factor_cmap(colour.value, palette=COLOURS, factors=df[colour.value].unique())
+
     # Draw points
-    if color.value != 'None':
-        p.circle(x=x.value, y=y.value, color=colors, line_color="white", size=9,
-                 alpha=0.6, hover_color='white', hover_alpha=0.5, source=source)
+    if colour.value != 'None':
+        p.circle(x=x.value, y=y.value, color=colours, line_color="white", size=9,
+                 alpha=0.6, hover_color='white', hover_alpha=0.5, source=source, 
+                 legend_field=colour.value)
+        
+        # Displaying legend
+        p.legend.orientation = "horizontal"
+        p.legend.location = "top_center"
     else:
         p.circle(x=x.value, y=y.value, color="#31AADE", line_color="white", size=9,
                  alpha=0.6, hover_color='white', hover_alpha=0.5, source=source)
-
     
     return p
 
@@ -93,12 +94,14 @@ x.on_change('value', update_figure)
 y = Select(title='Y-Axis', value='THE2020 Overall Score', options=numerical_cols)
 y.on_change('value', update_figure)
 
-color = Select(title='Color', value='None', options=['None'] + categorical_cols)
-color.on_change('value', update_figure)
+colour = Select(title='Colour', value='None', options=['None'] + categorical_cols)
+colour.on_change('value', update_figure)
 
 # Placement of bokeh controls
-controls = row(x, y, color, width=200)
+controls = row(x, y, colour, width=200)
 layout = column(controls, create_figure())
 
+
+curdoc().theme = "dark_minimal"
 curdoc().add_root(layout)
 curdoc().title = "520419832 Visualisation"
